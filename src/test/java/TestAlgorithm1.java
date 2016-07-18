a6e5dd6bad0249075719d141942449446d3473eb
mushuichuan <qqliys@126.com>
2016-07-18 14:25:37 +0800

    update algorithm1

--- src/test/java/TestAlgorithm1.java
+++ src/test/java/TestAlgorithm1.java
@@ -9,11 +9,31 @@ import static org.junit.Assert.assertEquals;
  */
 public class TestAlgorithm1 {
     @Test
-    public void test() {
+    public void test1() {
         int[] source = {2, 7, 11, 15};
         int target = 9;
         int[] solution = new Algorithm1().twoSum(source, target);
         int[] result = {0, 1};
+        assertArrayEquals(solution, result);
+
+    }
+
+    @Test
+    public void test2() {
+        int[] source = {2, -7, 11, -15};
+        int target = -13;
+        int[] solution = new Algorithm1().twoSum(source, target);
+        int[] result = {0, 3};
+        assertArrayEquals(solution, result);
+
+    }
+
+    @Test
+    public void test3() {
+        int[] source = {2, 7, 11, 15, 23, 45, 78, 99, 101, 802, 904, 978};
+        int target = 1882;
+        int[] solution = new Algorithm1().twoSum(source, target);
+        int[] result = {10, 11};
         assertArrayEquals(solution, result);
 
     }
