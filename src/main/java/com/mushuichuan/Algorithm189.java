eb6500a96952213105df3c9e6b711abf68c49e6a
mushuichuan <qqliys@126.com>
2016-07-19 14:21:04 +0800

    add algorithm189

---
+++ src/main/java/com/mushuichuan/Algorithm189.java
@@ -0,0 +1,49 @@
+package com.mushuichuan;
+
+/**
+ * Created by Liyanshun on 2016/7/19.
+ */
+public class Algorithm189 {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public void rotate2(int[] nums, int k) {
+        k = k % nums.length;
+        int[] temp = new int[k];
+        int index = 0;
+        for (int i = nums.length - k; i < nums.length; i++) {
+            temp[index++] = nums[i];
+        }
+        for (int i = nums.length - 1; i >= k; i--) {
+            nums[i] = nums[i - k];
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    }
+
+    public void rotate1(int[] nums, int k) {
+        k = k % nums.length;
+        for (int i = 0; i < k; i++) {
+            int temp = nums[nums.length - 1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = temp;
+        }
+    }
+}
