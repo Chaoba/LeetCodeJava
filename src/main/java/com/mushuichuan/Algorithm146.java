598991ff3d3d5fe12aefd4b2692f93a00e9dd3dd
mushuichuan <qqliys@126.com>
2016-07-20 10:54:21 +0800

    add some comments

--- src/main/java/com/mushuichuan/Algorithm146.java
+++ src/main/java/com/mushuichuan/Algorithm146.java
@@ -2,99 +2,97 @@ package com.mushuichuan;
 
 import java.util.HashMap;
 
+/**
+ * 146. LRU Cache  QuestionEditorial Solution  My Submissions
+ * Total Accepted: 78995
+ * Total Submissions: 498616
+ * Difficulty: Hard
+ * Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and set.
+ * <p>
+ * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+ * set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+ */
 public class Algorithm146 {
-	int currentSize = 0;
-	int max = 0;
-	Node head;
-	HashMap<Integer,Node> map=new HashMap<Integer, Node>();
+    int currentSize = 0;
+    int max = 0;
+    Node head;
+    HashMap<Integer, Node> map = new HashMap<Integer, Node>();
 
-	public Algorithm146(int capacity) {
-		max = capacity;
-	}
+    public Algorithm146(int capacity) {
+        max = capacity;
+    }
 
 
+    public int get(int key) {
+        if (head == null) {
+            return -1;
+        } else {
+            Node temp = map.get(key);
+            if (temp != null) {
+                // find the node, move it to head
+                if (temp != head) {
+                    // remove from current position
+                    temp.left.right = temp.right;
+                    temp.right.left = temp.left;
+                    // add to the head
+                    head.left.right = temp;
+                    temp.left = head.left;
+                    head.left = temp;
+                    temp.right = head;
+                    head = temp;
+                }
+                return temp.value;
+            } else {
+                return -1;
+            }
+        }
+    }
 
-	public int get(int key) {
-		if (head == null) {
-			return -1;
-		} else {
-			Node temp = map.get(key);
-			if (temp != null) {
-				// find the node, move it to head
-				if (temp != head) {
-					// remove from current position
-					temp.left.right = temp.right;
-					temp.right.left = temp.left;
-					// add to the head
-					head.left.right = temp;
-					temp.left = head.left;
-					head.left = temp;
-					temp.right = head;
-					head = temp;
-				}
-				return temp.value;
-			} else {
-				return -1;
-			}
-		}
-	}
+    public void set(int key, int value) {
+        if (head == null) {
+            // the first one
+            head = new Node(key, value);
+            head.left = head;
+            head.right = head;
+            currentSize++;
 
-	public void set(int key, int value) {
-		if (head == null) {
-			// the first one
-			head = new Node(key, value);
-			head.left = head;
-			head.right = head;
-			currentSize++;
+        } else if (get(key) == -1) {
+            // excess the capability
+            if (max == currentSize) {
+                map.remove(head.left.key);
+                head.left.key = key;
+                head.left.value = value;
+                head = head.left;
+            } else {
+                // add a new node to the list
+                currentSize++;
+                Node node = new Node(key, value);
+                node.left = head.left;
+                node.right = head;
+                head.left.right = node;
+                head.left = node;
+                head = node;
+            }
+        } else {
+            // replace current value
+            map.remove(head.key);
+            head.value = value;
+        }
 
-		} else if (get(key) == -1) {
-			// excess the capability
-			if (max == currentSize) {
-				map.remove(head.left.key);
-				head.left.key = key;
-				head.left.value = value;
-				head = head.left;
-			} else {
-				// add a new node to the list
-				currentSize++;
-				Node node = new Node(key, value);
-				node.left = head.left;
-				node.right = head;
-				head.left.right = node;
-				head.left = node;
-				head = node;
-			}
-		} else {
-			// replace current value
-			map.remove(head.key);
-			head.value = value;
-		}
+        map.put(key, head);
+    }
 
-		map.put(key, head);
-	}
+    class Node {
+        Node left;
+        Node right;
+        Node next;
+        int key;
+        int value;
 
-	public void test() {
-		System.out.println("");
-		Node temp = head;
-		do {
-			System.out.println(temp.key + ":" + temp.value);
-			temp = temp.right;
+        public Node(int k, int v) {
+            key = k;
+            value = v;
+        }
 
-		} while (temp != head);
-		System.out.println("");
-	}
-
-	class Node {
-		Node left;
-		Node right;
-		Node next;
-		int key;
-		int value;
-
-		public Node(int k, int v) {
-			key = k;
-			value = v;
-		}
-
-	}
+    }
 }
--- src/main/java/com/mushuichuan/Algorithm227.java
+++ src/main/java/com/mushuichuan/Algorithm227.java
@@ -1,47 +1,57 @@
 package com.mushuichuan;
 
+/**
+ * Implement a basic calculator to evaluate a simple expression string.
+ * <p>
+ * The expression string contains only non-negative integers, +, -, *, / operators and empty spaces . The integer division should truncate toward zero.
+ * <p>
+ * You may assume that the given expression is always valid.
+ * <p>
+ * Some examples:
+ * "3+2*2" = 7
+ * " 3/2 " = 1
+ * " 3+5 / 2 " = 5
+ */
 public class Algorithm227 {
-	public void test(){
-		System.out.println(calculate("100000000/1/2/3/4/5/6/7/8/9/10"));
-	}
-	public int calculate(String s) {
-		if (s == null || s.length() == 0) {
-			return 0;
-		}
 
-		int sumValue = 0;
-		int pre = 0, cur = 0;
-		char sign = '+';
-		int i = 0;
-		while (i < s.length()) {
-			cur = 0;
-			while (i < s.length() && ((s.charAt(i) >= '0' && s.charAt(i) <= '9') || s.charAt(i) == ' ')) {
-				if (s.charAt(i) == ' ') {
-					i++;
-					continue;
-				}
-				// is number
-				cur = cur * 10 + (s.charAt(i) - '0');
-				i++;
-			}
+    public int calculate(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int sumValue = 0;
+        int pre = 0, cur = 0;
+        char sign = '+';
+        int i = 0;
+        while (i < s.length()) {
+            cur = 0;
+            while (i < s.length() && ((s.charAt(i) >= '0' && s.charAt(i) <= '9') || s.charAt(i) == ' ')) {
+                if (s.charAt(i) == ' ') {
+                    i++;
+                    continue;
+                }
+                // is number
+                cur = cur * 10 + (s.charAt(i) - '0');
+                i++;
+            }
 //			System.out.println(cur);
-			if (sign == '+') {
-				sumValue += pre;
-				pre = cur;
-			} else if (sign == '-') {
-				sumValue += pre;
-				pre = -cur;
-			} else if (sign == '*') {
-				pre = pre * cur;
-			} else if (sign == '/') {
-				pre = pre / cur;
-			}
-			if (i < s.length()) {
-				sign = s.charAt(i);
-				i++;
-			}
+            if (sign == '+') {
+                sumValue += pre;
+                pre = cur;
+            } else if (sign == '-') {
+                sumValue += pre;
+                pre = -cur;
+            } else if (sign == '*') {
+                pre = pre * cur;
+            } else if (sign == '/') {
+                pre = pre / cur;
+            }
+            if (i < s.length()) {
+                sign = s.charAt(i);
+                i++;
+            }
 
-		}
-		return sumValue + pre;
-	}
+        }
+        return sumValue + pre;
+    }
 }
