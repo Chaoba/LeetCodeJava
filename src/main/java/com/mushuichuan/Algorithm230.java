968bbdfd1fa9e8875d48fc82d937eb6b0310bae1
mushuichuan <qqliys@126.com>
2016-07-24 16:11:44 +0800

    add algorithm226

--- src/main/java/com/mushuichuan/Algorithm230.java
+++ src/main/java/com/mushuichuan/Algorithm230.java
@@ -5,6 +5,14 @@ import com.mushuichuan.beans.TreeNode;
 import java.util.Stack;
 
 /**
+ * 230. Kth Smallest Element in a BST  QuestionEditorial Solution  My Submissions
+ * Total Accepted: 56155
+ * Total Submissions: 143050
+ * Difficulty: Medium
+ * Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+ * <p>
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
  * Created by Liyanshun on 2016/7/24.
  */
 public class Algorithm230 {
