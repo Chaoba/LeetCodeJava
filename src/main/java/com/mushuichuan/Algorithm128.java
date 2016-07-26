eef91d42405b2e6316223d7d2890adb18da4d590
mushuichuan <qqliys@126.com>
2016-07-26 16:36:42 +0800

    add algorithm108

--- src/main/java/com/mushuichuan/Algorithm128.java
+++ src/main/java/com/mushuichuan/Algorithm128.java
@@ -3,6 +3,17 @@ package com.mushuichuan;
 import java.util.HashMap;
 
 /**
+ * 128. Longest Consecutive Sequence  QuestionEditorial Solution  My Submissions
+ * Total Accepted: 71449
+ * Total Submissions: 214851
+ * Difficulty: Hard
+ * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+ * <p>
+ * For example,
+ * Given [100, 4, 200, 1, 3, 2],
+ * The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+ * <p>
+ * Your algorithm should run in O(n) complexity.
  * Created by Liyanshun on 2016/7/26.
  */
 public class Algorithm128 {
