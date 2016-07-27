f2c9d83cd5d24e5c237b7480698c2e349572f428
mushuichuan <qqliys@126.com>
2016-07-27 16:19:12 +0800

    add algorithm198

---
+++ src/test/java/TestAlgorithm198.java
@@ -0,0 +1,44 @@
+import com.mushuichuan.Algorithm198;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Liyanshun on 2016/7/18.
+ */
+public class TestAlgorithm198 {
+    @Test
+    public void test1() {
+        int[] source = {2, 7, 11, 15};
+
+        int solution = new Algorithm198().rob(source);
+        assertEquals(22, solution);
+
+    }
+
+    @Test
+    public void test2() {
+        int[] source = {2};
+
+        int solution = new Algorithm198().rob(source);
+        assertEquals(2, solution);
+
+    }
+    @Test
+    public void test3() {
+        int[] source = {2, 7};
+
+        int solution = new Algorithm198().rob(source);
+        assertEquals(7, solution);
+
+    }
+
+    @Test
+    public void test4() {
+        int[] source = {};
+
+        int solution = new Algorithm198().rob(source);
+        assertEquals(0, solution);
+
+    }
+}
